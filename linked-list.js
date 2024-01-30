ce9220583e84a15a2a2b49e3b41fa75454b20f56
Selin Uzunoglu <80995736+SelinWebSculptor@users.noreply.github.com>
2024-01-29 22:47:40 -0600

    phase-1

--- linked-list.js
+++ linked-list.js
@@ -7,15 +7,35 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here 
+    this.head = null;
+    this.length = 0
   }
 
   addToHead(val) {
-    // Your code here 
+    const newNode = new LinkedListNode(val)
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here 
+    const newNode = new LinkedListNode(val)
+
+    if (this.head === null) {
+      newNode.next = this.head;
+      this.head = newNode;
+
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next
+      }
+      current.next = newNode;
+    }
+    this.length++;
+
+
+    return newNode;
   }
 
   // You can use this function to help debug
